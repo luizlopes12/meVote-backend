74e2ce447a33675afd44104b75f311fecc1196d7
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-08-14 18:54:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const db = require('./config/database')
+const db = require('./config/database.js')
 const cors = require('cors')
 const routes = require('/Routes')
 const port = 3001
