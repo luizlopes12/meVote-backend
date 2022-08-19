81be3eeec60672f357a9669f651387ec3598e3a3
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-08-18 21:11:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const db = require('./config/database.js')
 const cors = require('cors')
 const routes = require('./routes/index.js')
+const Candidate = require('./models/Canditate.js')
 const port = process.env.PORT || 3000 
 const app = express()
 app.use(function(req, res, next) {
@@ -21,4 +22,18 @@ app.listen(port, () =>{
     console.log(`Servidor rodando, porta ${port}`)
 })
  
-// db.connection.sync({force:true})
+var dateObj = new Date();
+var month = dateObj.getUTCMonth() + 1; //months from 1-12
+var day = dateObj.getDate();
+var year = dateObj.getUTCFullYear();
+
+var newdate = year + "/" + month + "/" + day;
+if(newdate == '2022/8/21'){
+    db.connection.sync({force:true}).then(()=>{
+        for(let i = 1; i <= 7; i++){
+            Candidate.create({
+                label: i
+            })
+        }
+    })
+}
